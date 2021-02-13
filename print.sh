58258014dbc2ddec6b8dc2dbe401498bcf8934ce
Pierrick Greze <pierrick.greze@gmail.com>
2021-02-13 13:55:33 +0900

    Fix CI (#1)
    * Exposes junit results
    
    * Fix unit test results env
    
    * Dynamically generate the script used in tests
    
    * Some improvements

--- print.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-for arg in "$@"
-do
-    if [[ "$arg" == e=* ]]; then
-      echo "$arg" 1>&2
-    else
-      echo "$arg"
-    fi
-done
--- src/test/kotlin/com/github/pgreze/process/ProcessKtTest.kt
+++ src/test/kotlin/com/github/pgreze/process/ProcessKtTest.kt
@@ -20,14 +20,33 @@ import org.junit.jupiter.params.provider.ValueSource
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 import java.nio.file.Path
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.writeText
 
+@ExperimentalPathApi
 @ExperimentalCoroutinesApi
 class ProcessKtTest {
-    companion object {
+    private companion object {
         val OUT = arrayOf("hello world", "no worry")
         val ERR = arrayOf("e=omg", "e=windows")
         val ALL = arrayOf(OUT[0], ERR[0], OUT[1], ERR[1])
-        val CMD = arrayOf("./print.sh", *ALL)
+
+        fun Path.createScript(): Path = resolve("script.sh").also { f ->
+            val text = """
+                #!/usr/bin/env bash
+                for arg in "¥@"
+                do
+                    if [[ "¥arg" == e=* ]]; then
+                      echo 1>&2 "¥arg"
+                    else
+                      echo "¥arg"
+                    fi
+                done
+            """.trimIndent().replace("¥", "$") // https://stackoverflow.com/a/30699291/5489877
+            f.writeText(text)
+            f.toFile().setExecutable(true)
+        }
     }
 
     @Nested
@@ -35,7 +54,7 @@ class ProcessKtTest {
     inner class Print {
         @ParameterizedTest
         @ValueSource(booleans = [true, false])
-        fun test(print: Boolean) = runTestBlocking {
+        fun test(print: Boolean) = runSuspendTest {
             val mode = if (print) PRINT else SILENT
             val res = process("echo", "hello world", stdout = mode, stderr = mode)
 
@@ -46,7 +65,13 @@ class ProcessKtTest {
     }
 
     @Test
-    fun `env is allowing to inject environment variables`() = runTestBlocking {
+    fun `process support multiple arguments`() = runSuspendTest {
+        val output = process("echo", *OUT, stdout = CAPTURE).unwrap()
+        output shouldBeEqualTo listOf(OUT.joinToString(" "))
+    }
+
+    @Test
+    fun `env is allowing to inject environment variables`() = runSuspendTest {
         val name = "PROCESS_VAR"
         val value = "42"
         val output = process("env", env = mapOf(name to value), stdout = CAPTURE).unwrap()
@@ -54,12 +79,13 @@ class ProcessKtTest {
     }
 
     @Test
-    fun `process redirect to files`(@TempDir dir: Path) = runTestBlocking {
+    fun `process redirect to files`(@TempDir dir: Path) = runSuspendTest {
+        val script = dir.createScript()
         val errHeader = "bonjour"
         val out = dir.resolve("out.txt").toFile()
         val err = dir.resolve("err.txt").toFile().also { it.writeText("$errHeader\n") }
         process(
-            *CMD,
+            script.absolutePathString(), *ALL,
             stdout = ToFile(out, append = false),
             stderr = ToFile(err, append = true),
         ).unwrap()
@@ -72,10 +98,11 @@ class ProcessKtTest {
     @DisplayName("process with all outputs as capture is merging them")
     inner class CaptureAllOutputs {
         @RepeatedTest(3) // Repeat to ensure no random order.
-        fun test() = runTestBlocking {
+        fun test(@TempDir dir: Path) = runSuspendTest {
+            val script = dir.createScript()
             val consumer = ByteArrayOutputStream()
             val res = process(
-                *CMD,
+                script.absolutePathString(), *ALL,
                 stdout = CAPTURE,
                 stderr = CAPTURE,
                 consumer = PrintStream(consumer)::println,
@@ -88,10 +115,11 @@ class ProcessKtTest {
     }
 
     @Test
-    fun `use Consume when CAPTURE is unnecessary`() = runTestBlocking {
+    fun `use Consume when CAPTURE is unnecessary`(@TempDir dir: Path) = runSuspendTest {
+        val script = dir.createScript()
         val consumer = mutableListOf<String>()
         val output = process(
-            *CMD,
+            script.absolutePathString(), *ALL,
             stdout = Consume { it.toList(consumer) },
             stderr = CAPTURE,
         ).unwrap()
